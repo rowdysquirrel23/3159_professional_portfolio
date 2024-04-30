a564bbfa6816ff21e5848dd3e0cef0f4405b6b10
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-04-30 16:32:13 +0100

    Banner section of main page added

--- personal_site/src/pages/home/homePage.js
+++ personal_site/src/pages/home/homePage.js
@@ -1,9 +1,22 @@
 import './home.css';
+import main_img from '../../assets/portfolio_pic.jpg';
 
 export default function Home() {
 
     return (
-        <h1>Home</h1>
+        <div className='home_body'>
+            <section className='banner_content'>
+                <article className='personal_info'>
+                    <h1 className='portfolio_name'>Ben Scholefield - Web Developer</h1>
+                    <p className='portfolio_description'>
+                    I am at the end of a BSc (Hons) in Web Design & Development. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. 
+                    Suspendisse nec vehicula ipsum. Suspendisse molestie bibendum gravida. Duis mollis dolor id enim iaculis efficitur.
+                </p>
+                </article>
+                <img src={main_img} className='personal_img' alt='Man in a suit and sunglasses'></img>
+            </section>
+        </div>
     )
     
 }
