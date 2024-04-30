0cba0838d8a3312f9128d997a50c71400fd3ca7c
rowdysquirrel23 <24904759@edgehill.ac.uk>
2024-04-30 17:12:47 +0100

    Content added for second section of home page

--- personal_site/src/pages/home/homePage.js
+++ personal_site/src/pages/home/homePage.js
@@ -1,5 +1,8 @@
 import './home.css';
 import main_img from '../../assets/portfolio_pic.jpg';
+import github from '../../assets/github_icon.png';
+import linkedin from '../../assets/linkedin.png';
+import instagram from '../../assets/instagram.png';
 
 export default function Home() {
 
@@ -15,6 +18,40 @@ export default function Home() {
                 </p>
                 </article>
                 <img src={main_img} className='personal_img' alt='Man in a suit and sunglasses'></img>
+            </section>
+            <section className='home_content'>
+                <article className='card1'>
+                    <h2 className='crad1_title'>About Me</h2>
+                    <p className='card1_text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Vivamus scelerisque neque vel enim gravida venenatis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Vivamus scelerisque neque vel enim gravida venenatis.
+                    </p>
+                </article>
+                <article className='card2'>
+                    <div className='card2_link1'>
+                        <img src={github} alt='GitHub Icon'></img>
+                        <p>GitHub</p>
+                    </div>
+                    <div className='card2_link2'>
+                        <img src={linkedin} alt='linkedin icon'></img>
+                        <p>LinkedIn</p>
+                    </div>
+                    <div className='card2_link3'>
+                        <img src={instagram} alt='instagram icon'></img>
+                        <p>Instagram</p>
+                    </div>
+                </article>
+                <article className='card3'>
+                <h2 className='crad1_title'>Previous Work</h2>
+                    <p className='card1_text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Vivamus scelerisque neque vel enim gravida venenatis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Vivamus scelerisque neque vel enim gravida venenatis.
+                    </p>
+                </article>
             </section>
         </div>
     )
